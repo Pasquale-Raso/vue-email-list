51c2d7f40e5b995fcffaa17bc2d04e5d2d034fc8
Pasquale-Raso <82545398+Pasquale-Raso@users.noreply.github.com>
2021-09-01 15:48:02 +0200

    stampo una email

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,25 @@ Vue.config.devtools = true;
 
 const app = new Vue({
   el: "#app",
-  data: {},
+  data: {
+    mail: "",
+  },
   methods: {},
+  created() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((res) => {
+        // runs in case of success
+        const response = res.data.response;
+        console.log(response);
+        this.mail = response;
+      })
+      .catch((error) => {
+        // runs in case of error
+        console.log(error);
+      })
+      .then(() => {
+        // runs in any case
+      });
+  },
 });
